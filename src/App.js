0cea72873727098d6fe3175a3f1837e6cbacdaf3
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-19 17:12:22 +0300

    Added defaule state to navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Navbar from './component/layout/Navbar';
+import UserItem from './component/users/UserItem';
 import './App.css';
 
 class App extends Component{
   render(){
     return (
-      <nav className="navbar bg-primary">
-        <Navbar title="Github Finder" icon="fab fa-github"/>
-      </nav>
+      <div className="App">
+        <Navbar/>   
+      </div>
     );
   }
 }
