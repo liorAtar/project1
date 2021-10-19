5ebe093def205cad31298669553ab918e868c126
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-19 18:01:21 +0300

    added grid template to users

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <Navbar/>   
-        <Users/>
+        <Navbar/>  
+        <div className="container">
+          <Users/>
+        </div> 
       </div>
     );
   }
