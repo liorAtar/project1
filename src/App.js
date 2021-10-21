670891be8a98b4d41c4b662591942eb6442ad583
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-21 20:25:20 +0300

    added repos and repoItem components

--- src/App.js
+++ src/App.js
@@ -13,15 +13,23 @@ class App extends Component{
   state = {
     users: [], 
     user: {},
+    repos: [],
     loading: false, 
     alert: null
   }
-
+    
   // Get single Github user
   getUser = async (username) => {
     this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({user: res.data, loading: false});
+  }
+
+  // Get selected user's repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({repos: res.data, loading: false});
   }
 
   // Search requested user
@@ -43,7 +51,7 @@ class App extends Component{
   }
 
   render(){
-    const {users, user, loading} = this.state;
+    const {users, user, repos, loading} = this.state;
     return (
       <Router>
         <div className="App">
@@ -66,7 +74,7 @@ class App extends Component{
               )} />
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={this.getUser} user={user} loading={loading}/>
+                <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading}/>
               )} />
             </Switch>
           </div> 
