f1116743d89f18f3053d65d46104db0adc960a99
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-23 14:19:48 +0300

    changed all the classes to arrow functions

--- src/component/users/User.js
+++ src/component/users/User.js
@@ -1,46 +1,36 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 
-export class User extends Component {
-    componentDidMount(){
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    }
+    const {
+        name, 
+        company,
+        avatar_url, 
+        location, 
+        bio, 
+        blog, 
+        login, 
+        html_url, 
+        followers, 
+        following, 
+        public_repos, 
+        public_gists, 
+        hireable
+    } = user;
 
-    render() {
-
-        const {
-            name, 
-            company,
-            avatar_url, 
-            location, 
-            bio, 
-            blog, 
-            login, 
-            html_url, 
-            followers, 
-            following, 
-            public_repos, 
-            public_gists, 
-            hireable
-        } = this.props.user;
-
-        const {loading, repos} = this.props;
-
-        if(loading) return <Spinner />
-        
-        return <Fragment>
+    if(loading) return <Spinner />
+    
+    return (
+        <Fragment>
             <Link to="/" className="btn btn-light">Back To Search</Link>
             Hireable: {' '}
             {hireable? (
@@ -87,8 +77,16 @@ export class User extends Component {
             </div>
 
             <Repos repos={repos} />
-        </Fragment>;
-    }
+        </Fragment>
+    );
 }
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+};
 
 export default User;
